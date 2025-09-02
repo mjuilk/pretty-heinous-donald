06ca9d448377630909a295fa0e116367798d31e1
mjuilk <jonconneryl@gmail.com>
2025-09-02 16:14:06 +0200

    louef coord

--- rr_p1/tsv_in/louef_coord.pl
+++ rr_p1/tsv_in/louef_coord.pl
@@ -15,24 +15,30 @@ open(my $out, ">", "/Users/jon/Documents/mats/rsrcs/gnomad.v4.1.constraint_metri
 
 # Print the header line from the input file to the output file
 my $header = <$in>;
+chomp $header;
 print $out $header;
+
+# Get adaptors
+my $gene_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Human', 'Core', 'Gene');
+my $slice_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Human', 'Core', 'Slice');
 
 # Process each line in the input file
 while (my $line = <$in>) {
     chomp $line;
     my @fields = split(/\t/, $line);
-    my $gene_id = $fields[0];  # Assuming the gene ID is in the first column
+    my $gene_symbol = $fields[0];  # Gene symbol is in the first column
 
-    # Fetch the gene object from Ensembl
-    my $gene_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Human', 'Core', 'Gene');
-    my $gene = $gene_adaptor->fetch_by_stable_id($gene_id);
+    # Fetch all genes with this symbol
+    my $genes = $gene_adaptor->fetch_all_by_external_name($gene_symbol);
 
-    if ($gene) {
-        # Append the start and end positions to the line
+    if (@$genes) {
+        # Use the first gene if multiple exist (or handle as needed)
+        my $gene = $genes->[0];
         push @fields, $gene->start, $gene->end;
         print $out join("\t", @fields) . "\n";
     } else {
-        warn "Gene $gene_id not found in Ensembl\n";
+        # Log missing genes
+        warn "Gene symbol $gene_symbol not found in Ensembl\n";
     }
 }
 
