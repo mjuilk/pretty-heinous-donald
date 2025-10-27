af3ef460f58f560faf1479a5367ef755c527967c
mjuilk <jonconneryl@gmail.com>
2025-10-27 11:32:31 +0100

    added thresh var

--- rr_p1/cnv_z.py
+++ rr_p1/cnv_z.py
@@ -2,12 +2,17 @@ import numpy as np
 import pandas as pd
 
 # Read the TSV file
-df = pd.read_csv('/Users/jon/Documents/data/rer/out/c1/chr17_b.bed', sep='\t', header=None, names=['chr', 'start', 'end', 'coverage'])
+df = pd.read_csv('/Users/jon/Documents/data/rer/out/c4/test.bed', sep='\t',  dtype={'coverage': float})
 
 # Calculate z-scores for the 'coverage' column
 mean = df['coverage'].mean()
 stddev = df['coverage'].std()
 df['zscore'] = (df['coverage'] - mean) / stddev
+thresh = -4
+
+# Filter rows where zscore <= thresh
+filtered_df = df[df['zscore'] <= thresh]
 
 # Save the result to a new TSV file
-df.to_csv('/Users/jon/Documents/data/rer/out/c1/chr17_z.bed', sep='\t', index=False, header=False)
+df.to_csv('/Users/jon/Documents/data/rer/out/c4/c4_z.bed', sep='\t', index=False, header=True)
+filtered_df.to_csv('/Users/jon/Documents/data/rer/out/c4/c4_z4.bed', sep='\t', index=False, header=True)
