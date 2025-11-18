f0d5cfc0b4d62bd324f24e16a1ca40489568db60
mjuilk <jonconneryl@gmail.com>
2025-11-18 18:22:39 +0100

    added argparsing and sample sheet handling; need to modify fx next

--- rr_p1/csv_in/call_merge.py
+++ rr_p1/csv_in/call_merge.py
@@ -9,6 +9,7 @@
 # 5. add indicator columns for each caller       #
  ################################################
 
+import argparse
 import os
 import pandas as pd
 
@@ -18,19 +19,18 @@ def merger_sub(data, sam, mode):
     to be run inside of the main merger function
     '''
 
-    if mode in ["pu", "hq"]:
-        dedup = pd.concat([data['deep'], data['hap'], data['sen']]).drop_duplicates(["Chr", "Start", "Ref", "Alt"])
+    dedup = pd.concat([data['deep'], data['hap'], data['sen']]).drop_duplicates(["Chr", "Start", "Ref", "Alt"])
 
-        print(f"[DEDUP]\tNumber of variants : {dedup.shape[0]}")
+    print(f"[DEDUP]\tNumber of variants : {dedup.shape[0]}")
 
-        def make_key(df):
-            return df[['Chr', 'Start', 'Ref', 'Alt']].apply(tuple, axis=1)
+    def make_key(df):
+        return df[['Chr', 'Start', 'Ref', 'Alt']].apply(tuple, axis=1)
 
-        print("Generating keys")
-        key_unique = make_key(dedup)
-        key_dv = make_key(data['deep'])
-        key_hc = make_key(data['hap'])
-        key_st = make_key(data['sen'])
+    print("Generating keys")
+    key_unique = make_key(dedup)
+    key_dv = make_key(data['deep'])
+    key_hc = make_key(data['hap'])
+    key_st = make_key(data['sen'])
 
 #Add indicator columns
         print("Adding indicator columns...")
@@ -93,9 +93,50 @@ def merger(sam):
 
     merger_sub(hq_dict, sam, "hq")
 
+'''
 samples = [f"rr{i}" for i in range(1,22)]
 
 for sam in samples:
     os.chdir("/user/doad5844/results/reread_p1")
     print("\n===================================\n" sam + "\n===================================\n")
     merger(sam)
+'''
+
+#Argument parsing
+parser = argparse.ArgumentParser(description = "Python script for merging variant calls from samples (appropriate for Sentieon, DeepVariant, HaplotypeCaller, Manta, and TIDDIT) Provide path to a sample sheet as input (only argument).", epilog = "Output is a single merged CSV with columns indicating whether if it was found in the respective caller.")
+parser.add_argument("sheet", type=string, help = "Sample sheet containing paths for ANNOVAR output from different callers across samples (i.e. rows are samples, columns are [sample, caller1, caller2,...]")
+args = parser.parse_args()
+print(args.sheet)
+
+#Initialise log file and text
+with open("output.log", "w") as log:
+    log.write("LOG")
+
+log_text = ""
+
+#Sample sheet to pandas df
+sheet = pd.read_csv("args.sheet")
+samples = list(sheet["sample"])
+
+log_text += f"No. of Samples : {len(samples)} \n {samples}"
+
+#Loop through each row, drop empty columns (caller not used)
+for index, row in samples.iterrows():
+    row_series = row
+    row_series_cleaned = row_series[row_series != '']
+
+    sam_dict = {}
+    sam_dict = {
+             'deep' : pd.read_csv(sam + "_pude.csv"),
+             'hap' : pd.read_csv(sam + "_puha.csv"),
+             'sen' : pd.read_csv(sam + "_puse.csv")
+             }
+     print("=====PURE/NO QC=====\n")
+     print(f"[DEEP]\tNumber of variants : {pu_dict['deep'].shape[0]}")
+     print(f"[HAP]\tNumber of variants : {pu_dict['hap'].shape[0]}")
+     print(f"[SEN]\tNumber of variants : {pu_dict['sen'].shape[0]}")
+
+     merger_sub(pu_dict, sam, "pu")
+
+with open("output.log", "a") as log:
+    log.write(log_text)
